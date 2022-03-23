cb5d33fd124665ce4fef8bfc6fabd8bbe0b74017
CSK <467205498@qq.com>
2022-03-23 12:59:04 +0800

    modified:   push_pull.py

--- push_pull.py
+++ push_pull.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 # settings
 Load_Res = 4
 V_supply = 5000//2
-test_step = 1
 
 lst = []
-for i in range(0,V_supply,test_step):
+for i in range(0,V_supply,1):
     lst.append(((V_supply - i) * (i / Load_Res))/1000000)
 
 fig,ax=plt.subplots()
