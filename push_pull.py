151f1e81b2f57a6199ae015aeee88a0a5f86bbbb
CSK <467205498@qq.com>
2022-03-23 13:05:53 +0800

    modified:   push_pull.py

--- push_pull.py
+++ push_pull.py
@@ -1,8 +1,9 @@
+# 计算AB类功放每管的峰值耗散功率曲线
 import matplotlib.pyplot as plt
 
 # settings
-Load_Res = 4
-V_supply = 5000//2
+Load_Res = 2        #负载阻值(单位:Ω)
+V_supply = 5000//2  #电源电压(单位:mV,单电源须除以2)
 
 lst = []
 for i in range(0,V_supply,1):
