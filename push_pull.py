61e58653565fa3dac9b636ea998d342d4798fc4a
CSK <467205498@qq.com>
2022-03-25 10:23:14 +0800

    modified:   push_pull.py 	CSKmidi.py

--- push_pull.py
+++ push_pull.py
@@ -6,14 +6,23 @@ Load_Res = 2                            #负载阻值(单位:Ω)
 V_supply = 5000//2                      #电源电压(单位:mV,单电源须除以2)
 
 #计算
-lst = []
+lst_P = []
+lst_V = []
+lst_I = []
 for i in range(0,V_supply,1):
-    V_BJT = V_supply - i                #BJT电压(单位:mV)
-    I_BJT = i / Load_Res                #BJT电流(单位:mA)
-    P_BJT = (V_BJT*I_BJT)               #BJT瞬时耗散功率(单位:mW)
-    lst.append(P_BJT/1000000)           #转换单位
+    V_BJT = V_supply - i                #BJT瞬时电压(单位:mV)
+    I_BJT = i / Load_Res                #BJT瞬时电流(单位:mA)
+    P_BJT = V_BJT*I_BJT                 #BJT瞬时耗散功率(单位:mW)
+    P_BJT/=1000000                      #转换单位
+    V_BJT/=1000
+    I_BJT/=1000
+    lst_P.append(P_BJT)
+    lst_V.append(V_BJT)
+    lst_I.append(I_BJT)
 
 #作图
 fig,ax=plt.subplots()
-ax.plot(lst,c='red')
+ax.plot(lst_P,c='black')                #BJT瞬时耗散功率(单位:W)
+ax.plot(lst_V,c='red')                  #BJT瞬时电压(单位:V)
+ax.plot(lst_I,c='blue')                 #BJT瞬时电流(单位:mA)
 plt.show()
